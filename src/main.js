3d795693758d20d26c9db1c3d3247d176b3e60b2
Dominik Voss <vossdo99308@th-nuernberg.de>
2026-06-19 10:36:31 +0200

    Pfad angepasst

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 import './style.css';
 import { initStory } from './storytelling/scrollStory.js';
-import {initCharts} from './charts/charts.js';
+import { initCharts } from './charts/charts.js';
+
 
 async function start() {
 
-    const response =
-        await fetch('/data/umfrage.csv');
+    const response = await fetch(import.meta.env.BASE_URL + 'data/umfrage.csv');
 
-    const csv =
-        await response.text();
+    const csv = await response.text();
 
     initStory(csv);
     initCharts();
 }
+
 
 start();
